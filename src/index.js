47a6dddb0af984bfa3791ca32bb8af5c3c856559
ArtemDerenok <derenokartem95@gmail.com>
2023-07-14 14:30:21 +0300

    feat: add script

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
+import fetchAdvice from './api/api';
 import './scss/style.scss';
+
+const adviceNum = document.getElementById('number');
+const adviceText = document.getElementById('text');
+const btn = document.getElementById('button');
+const errorMsg = document.getElementById('error');
+
+const showAdvice = (data) => {
+  adviceNum.innerText = data.slip.id;
+  adviceText.innerText = data.slip.advice;
+};
+
+const showError = (error) => {
+  errorMsg.innerText = error.message;
+};
+
+const getAdvice = async () => {
+  const result = await fetchAdvice();
+  if (result instanceof Error) {
+    showError(result);
+  } else {
+    showAdvice(result);
+  }
+};
+
+getAdvice();
+
+btn.addEventListener('click', getAdvice);
